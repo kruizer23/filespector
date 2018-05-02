79b1a0a3953c2427a4678e9b9062182a23336062
Frank Voorburg <voorburg@gmx.net>
2018-05-02 20:53:19 +0200

    Configured "Release" as the default build mode when building via lazbuild and the makefile.

--- make.sh
+++ make.sh
@@ -4,12 +4,5 @@
 # Build the application.
 # 
 export LAZBUILD=$(which lazbuild)
-$LAZBUILD src/filecruncher.lpi
-
-
-#
-# Strip out symbols to shrink the size of the executable.
-# 
-strip src/filecruncher
-
+$LAZBUILD --build-mode="Release" src/filecruncher.lpi
  
