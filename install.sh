4ea2242dd711ef3853f94d77a5971957fa368e8a
Frank Voorburg <voorburg@gmx.net>
2018-05-29 00:01:33 +0200

    Added support for automatic (un)installing of all *.mo localization files.

--- install.sh
+++ install.sh
@@ -21,5 +21,10 @@ mkdir -p $DESTDIR/usr/share/filespector
 cp src/filespector.ico $DESTDIR/usr/share/filespector/
 mkdir -p $DESTDIR/usr/share/applications
 cp src/filespector.desktop $DESTDIR/usr/share/applications/
+for f in src/languages/*.mo; do 
+  LOCALE=$(echo "$f" | cut -d'.' -f2)
+  install -d -m 755 $DESTDIR/usr/share/locale/$LOCALE/LC_MESSAGES/
+	install -pm 644 "$f" $DESTDIR/usr/share/locale/$LOCALE/LC_MESSAGES/filespector.mo
+done
 
 
--- uninstall.sh
+++ uninstall.sh
@@ -19,4 +19,8 @@ rm -f $DESTDIR/usr/bin/filespector
 rm -f $DESTDIR/usr/share/filespector/filespector.ico
 rm -f $DESTDIR/usr/share/applications/filespector.desktop
 rmdir $DESTDIR/usr/share/filespector
+for f in src/languages/*.mo; do 
+  LOCALE=$(echo "$f" | cut -d'.' -f2)
+	rm -f $DESTDIR/usr/share/locale/$LOCALE/LC_MESSAGES/filespector.mo
+done
 
